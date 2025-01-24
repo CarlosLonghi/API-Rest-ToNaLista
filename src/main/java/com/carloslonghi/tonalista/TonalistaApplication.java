3e1fa610ffc985f3007c397247738b9daa909f40
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-01-24 10:23:28 -0300

    Initial Commit

---
+++ src/main/java/com/carloslonghi/tonalista/TonalistaApplication.java
@@ -0,0 +1,13 @@
+package com.carloslonghi.tonalista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonalistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonalistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloslonghi/tonalista/TonalistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloslonghi.tonalista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonalistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
