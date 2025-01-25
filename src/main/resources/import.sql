4656c0b853c55c78cbcd3a732a56ffab9413b00d
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-01-24 21:44:47 -0300

    feat: add `ItemList` and `Belonging` seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
+INSERT INTO tb_item_list (name) VALUES ('Lista de compras do Mês');
+INSERT INTO tb_item_list (name) VALUES ('Lista para o Churrasco');
+
+
 INSERT INTO tb_item (name, quantity, unit, status, priority) VALUES ('Arroz', 2.5, 'kg', 'PENDING', 'HIGH');
 INSERT INTO tb_item (name, quantity, unit, status, priority) VALUES ('Feijão', 1.5, 'kg', 'PURCHASED', 'MEDIUM');
 INSERT INTO tb_item (name, quantity, unit, status, priority) VALUES ('Macarrão', 1.0, 'kg', 'PENDING', 'LOW');
@@ -18,3 +22,26 @@ INSERT INTO tb_item (name, quantity, unit, status, priority) VALUES ('Refrigeran
 INSERT INTO tb_item (name, quantity, unit, status, priority) VALUES ('Água mineral', 10.0, 'l', 'PENDING', 'LOW');
 INSERT INTO tb_item (name, quantity, unit, status, priority) VALUES ('Laranja', 1.0, 'kg', 'PENDING', 'MEDIUM');
 INSERT INTO tb_item (name, quantity, unit, status, priority) VALUES ('Banana', 1.0, 'kg', 'PURCHASED', 'LOW');
+
+
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 1, 0);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 2, 1);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 3, 2);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 4, 3);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 5, 4);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 6, 0);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 7, 1);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 8, 2);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 9, 3);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (1, 10, 4);
+
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 11, 0);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 12, 1);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 13, 2);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 14, 3);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 15, 4);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 16, 0);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 17, 1);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 18, 2);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 19, 3);
+INSERT INTO tb_belonging (list_id, item_id, position) VALUES (2, 20, 4);
